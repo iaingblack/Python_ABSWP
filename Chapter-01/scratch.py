1cbfce361373a56e8fcd2a78dbd4eaeb02750a06
Iain Black <iaingblack@gmail.com>
2016-09-04 11:49:01 +0100

    Chapter2

--- Chapter-01/scratch.py
+++ Chapter-01/scratch.py
@@ -1,9 +1,8 @@
-print('tesst')
-
 name = input("What is your name?")
 print("Hi " + name)
 print("Length of name is " + str(len(name)))
-
+age = input("What is your age? ")
+print("You will be " + str(int(age) + 1) + " in a year")
 
 
 
---
+++ Chapter-02/scratch.py
@@ -0,0 +1,10 @@
+spam=3
+
+if spam == 1:
+    print('Hello')
+elif spam == 2:
+    print('Howdy')
+else:
+    print('Greetings!')
+
+
