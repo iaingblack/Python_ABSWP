a292b731eec8895b55ff93d9a030c7a4a65e0b85
Iain Black <iaingblack@gmail.com>
2016-09-04 13:13:20 +0100

    Tweaked to give total number of iterations

--- Chapter-03/p1-collatz.py
+++ Chapter-03/p1-collatz.py
@@ -1,11 +1,15 @@
 def collatz(number):
+    iterations = 0
     while number != 1:
+        iterations += 1
         if number % 2 == 0:
             number = number // 2
             print(str(number))
         else:
             number = (number * 3 + 1)
             print(str(3 * number + 1))
+
+    print("Iterations was " + str(iterations))
 
 userNum = int(input("Enter a number: "))
 collatz(userNum)
